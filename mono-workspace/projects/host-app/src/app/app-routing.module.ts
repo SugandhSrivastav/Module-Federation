d2d65382022992acb72615430138da801c652fc1
Sugandh <sugandh800@gmail.com>
2023-08-30 19:06:52 +0530

    Module Federation Implementation

---
+++ mono-workspace/projects/host-app/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+
+const MFE_APP_URL = "http://localhost:4300/remoteEntry.js";
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {
+    path: 'todo-list', 
+    loadChildren: () => {
+      return loadRemoteModule({
+        remoteEntry: MFE_APP_URL,
+        remoteName: "mfeApp",
+        exposedModule: "./TodoListModule"
+      }).then(m => m.TodoListModule).catch(err => console.log(err));
+    }
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+export const routeCompArr = [HomeComponent];
